84b92883958594419a9091236b6a1937f4e77639
Riccardo Albertazzi <piano.vergato@gmail.com>
2019-04-20 16:10:42 +0200

    Adding bad formatted black function

--- life/questions.py
+++ life/questions.py
@@ -7,3 +7,11 @@ def the_answer_to_life_the_universe_and_everything():
 
 def the_answer_to_life_the_universe_and_everything_using_numpy():
     return np.array([42])
+
+
+def a_bad_black_formatted_list():
+    return [
+        1,
+        2,
+        3
+    ]
