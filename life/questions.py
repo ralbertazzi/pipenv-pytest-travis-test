11487948330f5e269b0a3c4ad955ec9adfd9681c
Riccardo Albertazzi <piano.vergato@gmail.com>
2019-04-20 15:37:20 +0200

    Code commit

---
+++ life/questions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def the_answer_to_life_the_universe_and_everything():
+    return 42
+
+
+def the_answer_to_life_the_universe_and_everything_using_numpy():
+    return np.array([42])
---
+++ tests/test_questions.py
@@ -0,0 +1,9 @@
+from life import questions
+
+
+def test_life_answer_to_life_the_universe_and_everything():
+    assert questions.the_answer_to_life_the_universe_and_everything() == 42
+
+
+def test_life_answer_to_life_the_universe_and_everything_using_numpy():
+    assert len(questions.the_answer_to_life_the_universe_and_everything_using_numpy()) == 1
