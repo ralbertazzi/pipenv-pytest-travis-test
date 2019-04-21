a18bddba39b4ae716639ac5b6437f8acf5a307fb
Riccardo Albertazzi <piano.vergato@gmail.com>
2019-04-21 17:34:25 +0200

    Added DangerJS to CI

--- life/questions.py
+++ life/questions.py
@@ -10,8 +10,4 @@ def the_answer_to_life_the_universe_and_everything_using_numpy():
 
 
 def a_bad_black_formatted_list():
-    return [
-        1,
-        2,
-        3
-    ]
+    return [1, 2, 3]
